e1ca2a51ec8db7f9fd89b777b588f0ad06bd887f
khandelwalbrijesh <khandelwalbrijeshiitr@gmail.com>
2019-06-13 22:50:35 +0530

    Added KillService.ps1 file

---
+++ src/PatchOrchestrationApplication/NodeAgentService/src/KillService.ps1
@@ -0,0 +1,10 @@
+﻿$id = Get-WmiObject -Class Win32_Service -Filter "Name LIKE 'POSNodeSvc'" | Select-Object -ExpandProperty ProcessId
+if($id -ne $null)
+{
+   $process = Get-Process -Id $id
+   if($process.Id -ne $null)
+   {
+      taskkill /F /PID $process.Id
+   }
+}
+
