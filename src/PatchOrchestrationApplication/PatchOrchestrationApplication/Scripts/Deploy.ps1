9ffb2cad6f4c25f28cd562e44afa7c17cd8dcef3
Saket Harsh <saharsh@microsoft.com>
2020-03-30 12:31:02 +0530

    Version update for POA

--- src/PatchOrchestrationApplication/PatchOrchestrationApplication/Scripts/Deploy.ps1
+++ src/PatchOrchestrationApplication/PatchOrchestrationApplication/Scripts/Deploy.ps1
@@ -11,7 +11,7 @@ Param
     $ImageStoreConnectionString = "fabric:ImageStore",
 
     [string]
-    $ApplicationVersion = "1.4.3",
+    $ApplicationVersion = "1.4.4",
 	
     [hashtable]
     $ApplicationParameters = @{}
